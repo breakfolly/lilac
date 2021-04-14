495785aba49edc36756004686e05d72e485c0471
breakfolly <btwn713@gmail.com>
2021-04-14 21:10:53 +0900

    feat: Running server in background

--- plugin/lilac.py
+++ plugin/lilac.py
@@ -1,20 +1,22 @@
+#!/usr/bin/env python3
 #-*- coding:utf-8 -*-
+
 import socket
+import argparse
 
-def print_test():
-    print ("Hello from Python Function \'print_test\'")
-
-def print_test2(arg1="text"):
-    print (arg1)
+parser = argparse.ArgumentParser(description='Start a Lilac server.')
+parser.add_argument('port', type=int, nargs='?', help='Port number to run on')
+parser.add_argument('name', type=str, nargs='?', help='Your name to run on')
 
 class LilacServer():
-    def __init__(self, host, port, name):
-        self.host = host
+    def __init__(self, port, name):
+        self.host = "127.0.0.1"
         self.port = int(port)
         self.name = name
         self.addr = (self.host, self.port)
 
         self.size = 1024
+        self.isConnected = True
         self.print_info()
         self.open_server()
     
@@ -26,13 +28,16 @@ class LilacServer():
             server_socket.bind(self.addr)
             server_socket.listen()
 
-            while True:
+            while self.isConnected:
                 client_socket, client_addr = server_socket.accept()
                 msg = client_socket.recv(self.size)
                 print ("[{}] msg : {}".format(client_addr, msg))
 
                 client_socket.sendall("welcome!".encode())
                 client_socket.close()
+
+    def quit(self):
+        self.isConnected = False
 
 class LilacClient():
     def __init__(self, host, port, name):
@@ -54,3 +59,8 @@ class LilacClient():
             client_socket.send("hi".encode())
             msg = client_socket.recv(self.size)
             print ("resp from server : {}".format(msg))
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print (args)
+    Server = LilacServer(args.port, args.name)
--- plugin/lilac.vim
+++ plugin/lilac.vim
@@ -5,8 +5,7 @@ endif
 
 let s:plugin_root_dir = fnamemodify(resolve(expand('<sfile>:p')), ':h')
 
-command! -nargs=* Lilac python3 lilac.LilacServer(<f-args>)
-command! -nargs=* LilacClient python3 lilac.LilacClient(<f-args>)
+command! -nargs=* Lilac python3 command(<f-args>)
 
 python3 << EOF
 
@@ -15,7 +14,17 @@ from os.path import normpath, join
 import vim
 plugin_root_dir = vim.eval('s:plugin_root_dir')
 sys.path.insert(0, plugin_root_dir)
+LilacPyPath = plugin_root_dir + "/lilac.py"
 import lilac
+
+def command(arg1, arg2="127.0.0.1", arg3="1111", arg4="user"):
+	if arg1 == "connect":
+		lilac.LilacClient(arg2, arg3, arg4)
+	elif arg1 == "start":	
+		vim.command(':silent execute "!' + LilacPyPath + ' ' + arg3 + ' ' + arg4 + '&>/dev/null &" | execute ":redraw!"')
+	#	vim.command(':silent execute "!' + LilacPyPath + ' ' + arg3 + ' ' + arg4 + '" | execute ":redraw!"')
+	elif arg1 == "quit":
+	    lilac.LilacServer.quit()
 
 EOF
 
